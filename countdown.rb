99092e7b21527b747e80cf81bc219b6e1d336f16
namimi928 <soyongnam@gmail.com>
2016-01-28 16:18:25 -0500

    no message

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(num)
+
+index = num
+loop do 
+  puts "#{num} SECOND(S)!\n"
+  num -= 1
+  break if num == 0
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+ sleep(num)
 end
